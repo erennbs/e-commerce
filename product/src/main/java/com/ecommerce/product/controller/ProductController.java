548a3dc5cdb2b4f414714fbd72d6e4be48a191b5
Eren <erenbas3453@gmail.com>
2026-02-05 19:15:11 +0300

    Add product controller

---
+++ product/src/main/java/com/ecommerce/product/controller/ProductController.java
@@ -0,0 +1,50 @@
+package com.ecommerce.product.controller;
+
+import com.ecommerce.product.dto.ProductPurchaseRequest;
+import com.ecommerce.product.dto.ProductPurchaseResponse;
+import com.ecommerce.product.dto.ProductRequestDto;
+import com.ecommerce.product.dto.ProductResponseDto;
+import com.ecommerce.product.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/product")
+@AllArgsConstructor
+public class ProductController {
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDto>> getProducts() {
+        return ResponseEntity.ok(productService.getProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> getProducts(@PathVariable UUID id) {
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> crateProduct(@Valid @RequestBody ProductRequestDto request) {
+        return ResponseEntity.ok(productService.createProduct(request));
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<List<ProductPurchaseResponse>> purchaseProducts(
+            @Valid @RequestBody List<ProductPurchaseRequest> request
+    ) {
+        return ResponseEntity.ok(productService.purchaseProducts(request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable UUID id) {
+        productService.deleteProduct(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
