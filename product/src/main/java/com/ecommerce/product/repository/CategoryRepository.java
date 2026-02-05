5882717fefb5aaa8002f74bc141094669df60ec1
Eren <erenbas3453@gmail.com>
2026-02-05 15:35:43 +0300

    Add product and category repositories

---
+++ product/src/main/java/com/ecommerce/product/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.ecommerce.product.repository;
+
+import com.ecommerce.product.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
