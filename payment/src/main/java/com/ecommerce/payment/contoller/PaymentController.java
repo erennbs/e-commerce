bce2975236e757266d645dfbaa10ddcf611115a0
Eren <erenbas3453@gmail.com>
2026-02-11 21:35:22 +0300

    Add payment controller

---
+++ payment/src/main/java/com/ecommerce/payment/contoller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.ecommerce.payment.contoller;
+
+import com.ecommerce.payment.dto.PaymentRequest;
+import com.ecommerce.payment.dto.PaymentResponse;
+import com.ecommerce.payment.model.Payment;
+import com.ecommerce.payment.service.PaymentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/payments")
+@RequiredArgsConstructor
+public class PaymentController {
+    private final PaymentService paymentService;
+
+    @PostMapping
+    public ResponseEntity<PaymentResponse> createPayment(@RequestBody @Valid PaymentRequest paymentRequest) {
+        return ResponseEntity.ok(paymentService.createPayment(paymentRequest));
+    }
+}
