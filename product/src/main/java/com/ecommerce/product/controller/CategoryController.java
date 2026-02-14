334a3d55c524482b06219dfb18e7942a9bec6d42
Eren <erenbas3453@gmail.com>
2026-02-14 19:15:47 +0300

    Fix api routes naming

--- product/src/main/java/com/ecommerce/product/controller/CategoryController.java
+++ product/src/main/java/com/ecommerce/product/controller/CategoryController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/category")
+@RequestMapping("api/v1/categories")
 @AllArgsConstructor
 public class CategoryController {
 
--- product/src/main/java/com/ecommerce/product/controller/ProductController.java
+++ product/src/main/java/com/ecommerce/product/controller/ProductController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/v1/product")
+@RequestMapping("api/v1/products")
 @AllArgsConstructor
 public class ProductController {
     private ProductService productService;
