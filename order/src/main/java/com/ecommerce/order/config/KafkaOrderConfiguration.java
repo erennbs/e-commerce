2b6ef81fda3bac05ac3b628ce46e97756b10c20c
Eren <erenbas3453@gmail.com>
2026-02-16 20:45:03 +0300

    Remove order service kafka bean config

--- order/src/main/java/com/ecommerce/order/config/KafkaOrderConfiguration.java
+++ order/src/main/java/com/ecommerce/order/config/KafkaOrderConfiguration.java
@@ -1,19 +1,10 @@
 package com.ecommerce.order.config;
 
-import com.ecommerce.order.kafka.OrderConfirmation;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
-
-import java.util.HashMap;
 
 @Configuration
 public class KafkaOrderConfiguration {
@@ -25,20 +16,5 @@ public class KafkaOrderConfiguration {
         return TopicBuilder
                 .name("order-topic")
                 .build();
-    }
-
-    @Bean
-    public ProducerFactory<String, OrderConfirmation> orderProducerFactory() {
-        HashMap<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
-
-        return new DefaultKafkaProducerFactory<>(config);
-    }
-
-    @Bean
-    public KafkaTemplate<String, OrderConfirmation> orderKafkaTemplate() {
-        return new KafkaTemplate<>(orderProducerFactory());
     }
 }
