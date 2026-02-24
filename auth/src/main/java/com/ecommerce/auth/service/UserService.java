824b6bbad7171cb7ca2dd201211660116b335128
Eren <erenbas3453@gmail.com>
2026-02-24 20:14:03 +0300

    Add user repository and service

---
+++ auth/src/main/java/com/ecommerce/auth/service/UserService.java
@@ -0,0 +1,18 @@
+package com.ecommerce.auth.service;
+
+import com.ecommerce.auth.model.User;
+import com.ecommerce.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
